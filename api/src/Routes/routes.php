c0af591b3657df5b0e06bb32f19b041afd899bbf
Manuel <caballeromanuel203@gmail.com>
2025-04-28 23:31:07 +0800

    added 404 handler

--- api/src/Routes/routes.php
+++ api/src/Routes/routes.php
@@ -6,3 +6,8 @@ use Src\Core\Router;
 $router = new Router(base: "/api/v1");
 
 $router->resource("products", ProductController::class);
+
+$router->_404(function () {
+    status(404);
+    return json(["message" => "Requested resource not found."]);
+});
