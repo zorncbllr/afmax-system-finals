a22fa34d9bc2daa84d0d5c70f65c5ff6cf176a8a
Manuel <caballeromanuel203@gmail.com>
2025-04-30 21:48:47 +0800

    added hero section layout

---
+++ client/src/components/ui/hero-parallax.tsx
@@ -0,0 +1,177 @@
+import {
+  motion,
+  useScroll,
+  useTransform,
+  useSpring,
+  MotionValue,
+} from "motion/react";
+import React from "react";
+import { Link } from "react-router";
+import { Button } from "./button";
+
+export const HeroParallax = ({
+  products,
+}: {
+  products: {
+    title: string;
+    link: string;
+    thumbnail: string;
+  }[];
+}) => {
+  const firstRow = products.slice(0, 5);
+  const secondRow = products.slice(5, 10);
+  const thirdRow = products.slice(10, 15);
+  const ref = React.useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  });
+
+  const springConfig = { stiffness: 300, damping: 30, bounce: 100 };
+
+  const translateX = useSpring(
+    useTransform(scrollYProgress, [0, 1], [0, 1000]),
+    springConfig
+  );
+  const translateXReverse = useSpring(
+    useTransform(scrollYProgress, [0, 1], [0, -1000]),
+    springConfig
+  );
+  const rotateX = useSpring(
+    useTransform(scrollYProgress, [0, 0.2], [15, 0]),
+    springConfig
+  );
+  const opacity = useSpring(
+    useTransform(scrollYProgress, [0, 0.2], [0.2, 1]),
+    springConfig
+  );
+  const rotateZ = useSpring(
+    useTransform(scrollYProgress, [0, 0.2], [20, 0]),
+    springConfig
+  );
+  const translateY = useSpring(
+    useTransform(scrollYProgress, [0, 0.2], [-700, 500]),
+    springConfig
+  );
+  return (
+    <div
+      ref={ref}
+      className="h-[300vh] py-40 overflow-hidden  antialiased relative flex flex-col self-auto [perspective:1000px] [transform-style:preserve-3d]"
+    >
+      <Header />
+      <motion.div
+        style={{
+          rotateX,
+          rotateZ,
+          translateY,
+          opacity,
+        }}
+        className=""
+      >
+        <motion.div className="flex flex-row-reverse space-x-reverse space-x-20 mb-20">
+          {firstRow.map((product) => (
+            <ProductCard
+              product={product}
+              translate={translateX}
+              key={product.title}
+            />
+          ))}
+        </motion.div>
+        <motion.div className="flex flex-row  mb-20 space-x-20 ">
+          {secondRow.map((product) => (
+            <ProductCard
+              product={product}
+              translate={translateXReverse}
+              key={product.title}
+            />
+          ))}
+        </motion.div>
+        <motion.div className="flex flex-row-reverse space-x-reverse space-x-20">
+          {thirdRow.map((product) => (
+            <ProductCard
+              product={product}
+              translate={translateX}
+              key={product.title}
+            />
+          ))}
+        </motion.div>
+      </motion.div>
+    </div>
+  );
+};
+
+export const Header = () => {
+  return (
+    <section className="mx-auto max-w-2xl pt-54 relative z-20">
+      <div className="flex justify-center">
+        <div className="relative rounded-full mb-8 px-3 py-1 text-sm/6 w-fit text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+          Latest medical supplies available.
+          <Link to="#" className="font-semibold text-sky-600">
+            <span className="absolute inset-0" aria-hidden="true"></span>See
+            what's new
+            <span aria-hidden="true">&rarr;</span>
+          </Link>
+        </div>
+      </div>
+      <div className="text-center">
+        <h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
+          Trusted Medical Supplies,
+          <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+            Delivered!
+          </span>
+        </h1>
+        <p className="mt-8 text-lg font-medium text-pretty text-gray-700 sm:text-xl/8">
+          Order high-quality medical supplies with ease and get them delivered
+          to your hospital or clinic on time.
+        </p>
+        <div className="mt-10 flex items-center justify-center gap-x-6">
+          <Button className="rounded-md px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs bg-gradient-to-r to-emerald-600 from-sky-400">
+            See Products
+          </Button>
+          <Button variant="secondary">
+            Custom Order <span aria-hidden="true">→</span>
+          </Button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export const ProductCard = ({
+  product,
+  translate,
+}: {
+  product: {
+    title: string;
+    link: string;
+    thumbnail: string;
+  };
+  translate: MotionValue<number>;
+}) => {
+  return (
+    <motion.div
+      style={{
+        x: translate,
+      }}
+      whileHover={{
+        y: -20,
+      }}
+      key={product.title}
+      className="group/product h-96 w-[30rem] relative shrink-0"
+    >
+      <a href={product.link} className="block group-hover/product:shadow-2xl ">
+        <img
+          src={product.thumbnail}
+          height="600"
+          width="600"
+          className="object-cover object-left-top absolute h-full w-full inset-0"
+          alt={product.title}
+        />
+      </a>
+      <div className="absolute inset-0 h-full w-full opacity-0 group-hover/product:opacity-80 bg-black pointer-events-none"></div>
+      <h2 className="absolute bottom-4 left-4 opacity-0 group-hover/product:opacity-100 text-white">
+        {product.title}
+      </h2>
+    </motion.div>
+  );
+};
