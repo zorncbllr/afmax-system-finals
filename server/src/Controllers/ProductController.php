07ba8b58fd44728719bdb22e09c817cf36cf50e0
Manuel <caballeromanuel203@gmail.com>
2025-05-07 05:23:58 +0800

    moved validation in controllers

--- server/src/Controllers/ProductController.php
+++ server/src/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace Src\Controllers;
 
+use Rakit\Validation\Validator;
 use Src\Core\App;
 use Src\Core\Exceptions\ServiceException;
 use Src\Core\Interfaces\IResource;
@@ -48,13 +49,36 @@ class ProductController implements IResource
     public function create(Request $request)
     {
         try {
-            $errors = $this->productService->createProduct($request);
+            $validator = new Validator();
 
-            if ($errors) {
+            $data = [
+                'productName' => $request->productName,
+                'brand' => $request->brand,
+                'description' => $request->description,
+                'price' => $request->price,
+                'categories' => $request->categories,
+                'images' => $request->files->images->name,
+            ];
 
+            $rules = [
+                'productName' => 'required|min:3|max:100',
+                'brand'       => 'required|min:2|max:50',
+                'description' => 'required|min:10|max:10000',
+                'price'       => 'required|numeric|min:0.01',
+                'categories'  => 'required|array|min:1',
+                'categories.*' => 'required|regex:/^[a-zA-Z0-9\s]+$/|min:2|max:30',
+                'images'      => 'required|array|min:1|max:5',
+                'images.*'    => 'uploaded_file|max:2M|mimes:jpeg,png,gif'
+            ];
+
+            $validation = $validator->validate($data, $rules);
+
+            if ($validation->fails()) {
                 status(400);
-                return json($errors);
+                return json($validation->errors()->firstOfAll());
             }
+
+            $this->productService->createProduct($request);
 
             status(200);
             return json(["message" => "New product has been created."]);
--- server/src/Services/ProductService.php
+++ server/src/Services/ProductService.php
@@ -3,7 +3,6 @@
 namespace Src\Services;
 
 use PDOException;
-use Rakit\Validation\Validator;
 use Src\Core\Database;
 use Src\Core\Exceptions\ServiceException;
 use Src\Core\Request;
@@ -52,34 +51,6 @@ class ProductService
     public function createProduct(Request $request)
     {
         try {
-            $validator = new Validator();
-
-            $data = [
-                'productName' => $request->productName,
-                'brand' => $request->brand,
-                'description' => $request->description,
-                'price' => $request->price,
-                'categories' => $request->categories,
-                'images' => $request->files->images->name,
-            ];
-
-            $rules = [
-                'productName' => 'required|min:3|max:100',
-                'brand'       => 'required|min:2|max:50',
-                'description' => 'required|min:10|max:10000',
-                'price'       => 'required|numeric|min:0.01',
-                'categories'  => 'required|array|min:1',
-                'categories.*' => 'required|regex:/^[a-zA-Z0-9\s]+$/|min:2|max:30',
-                'images'      => 'required|array|min:1|max:5',
-                'images.*'    => 'uploaded_file|max:2M|mimes:jpeg,png,gif'
-            ];
-
-            $validation = $validator->validate($data, $rules);
-
-            if ($validation->fails()) {
-                return $validation->errors()->firstOfAll();
-            }
-
             $product = new Product();
 
             $product->productName = $request->productName;
